00a88afd4c3c9a4588328684350969db238d5de3
Markus Punnar <markus.punnar@gmail.com>
2022-02-06 17:38:31 +0200

    Remove RecoilRoot

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import BudgetApp from "./BudgetApp";
-import { RecoilRoot } from "recoil";
 
 ReactDOM.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <BudgetApp />
-    </RecoilRoot>
+    <BudgetApp />
   </React.StrictMode>,
   document.getElementById("root")
 );
